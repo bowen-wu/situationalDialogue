818ccb403e2e2223a1602903515a3a298cc468dc
bowen-wu <13793248162@163.com>
2018-05-26 19:39:35 +0800

    change to vue

---
+++ src/components/Dialog.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="dialog">
+    
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  data () {
+    return {
+      tabList: [
+          {
+
+          }
+      ]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+@import "@/assets/css/reset.scss"
+
+</style>
